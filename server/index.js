cddd5d01b538e9275918873dc89b65ad6db459f6
clydebates <a.bates1993@gmail.com>
2019-08-03 15:28:00 -0500

    Trying to get started with Plaid

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const plaid = require('plaid');
 
 /**
  * express required to aid in in handeling request made to server
@@ -21,13 +22,33 @@ const app = express();
  * middleware assigned to app for use with incoming requests
  */
 
-app.use(express.static(path.join(__dirname, '../dist')));
+// app.use(express.static(path.join(__dirname, '../dist')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'anything' }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(session({ secret: 'anything' }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
+
+let ACCESS_TOKEN = null;
+let PUBLIC_TOKEN = null;
+
+const client = new plaid.Client(
+  process.env.PLAID_CLIENT_ID,
+  process.env.PLAID_SECRET,
+  process.env.PLAID_PUBLIC_KEY,
+  plaid.environments.sandbox
+);
+
+app.post('https://sandbox.plaid.com/item/public_token/exchange', {
+  client_id: process.env.PLAID_CLIENT_ID,
+  secret: process.env.PLAID_SECRET,
+  public_token: process.env.PLAID_PUBLIC_KEY,
+})
+.then((response) => {
+  console.log(response);
+})
+.catch((err) => console.error(err));
 
 
 const PORT = 8080;

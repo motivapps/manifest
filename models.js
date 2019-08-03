eb454181eeb8ecddf91c26dfc98e46c3b96cec06
Charles Teamer <cmo1462@gmail.com>
2019-08-03 10:42:55 -0500

    installed dotenv

---
+++ models.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DATABASE, 
+  process.env.DB_USERNAME, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.NGROK,
+    dialiect: 'postgresql',
+  }
+)
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });

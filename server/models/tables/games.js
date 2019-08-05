720a83a560e107df6691a117160aeb698e1a51e3
clydebates <a.bates1993@gmail.com>
2019-08-05 12:09:09 -0500

    Delete server code

--- server/models/tables/games.js
+++
@@ -1,9 +0,0 @@
-const games = (sequelize, DataTypes) => {
-  const Game = sequelize.define('games', {
-    "game": DataTypes.STRING,
-  }, { sequelize })
-  
-  return Game;
-}
-
-module.exports = games;

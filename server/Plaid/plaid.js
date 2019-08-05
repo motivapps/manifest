3e24889bd6943b6d9ca0ac4becd54e0b595b5422
clydebates <a.bates1993@gmail.com>
2019-08-05 09:45:40 -0500

    Add plaid file, may or may not need it

--- server/Plaid/plaid.js
+++ server/Plaid/plaid.js
@@ -1,7 +1,28 @@
-require('dotenv').config();
-const express = require('express');
+// This entire file may or may not be unnecessary, unsure right now
+
+// require('dotenv').config();
+// const express = require('express');
+
+// let ACCESS_TOKEN = null;
+// let PUBLIC_TOKEN = null;
+
+// const client = new plaid.Client(
+//   process.env.PLAID_CLIENT_ID,
+//   process.env.PLAID_SECRET,
+//   process.env.PLAID_PUBLIC_KEY,
+//   plaid.environments.sandbox
+// );
+
+// axios.post('https://sandbox.plaid.com/item/public_token/exchange', {
+//   "client_id": process.env.PLAID_CLIENT_ID,
+//   "secret": process.env.PLAID_SECRET,
+//   "public_token": "public-sandbox-3ef5a951-f351-4fc3-9073-5d3934397a10"
+// })
+// .then((response) => {
+//   console.log(response);
+// })
+// .catch((err) => console.error(err));
 
 
 
-
-const app = 
+// const app = 

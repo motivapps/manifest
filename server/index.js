00003f05c4a2a401617ff47c3238dfd9c0487442
clydebates <a.bates1993@gmail.com>
2019-08-05 09:46:30 -0500

    Slight linting fixes

--- server/index.js
+++ server/index.js
@@ -31,35 +31,14 @@ app.use(bodyParser.json());
 // app.use(passport.session());
 
 app.get('http://127.0.0.1:4041', (req, res) => {
-    console.log('inside app get');
-    console.log('req:', req);
-})
-
-let ACCESS_TOKEN = null;
-let PUBLIC_TOKEN = null;
-
-const client = new plaid.Client(
-  process.env.PLAID_CLIENT_ID,
-  process.env.PLAID_SECRET,
-  process.env.PLAID_PUBLIC_KEY,
-  plaid.environments.sandbox
-);
-
-axios.post('https://sandbox.plaid.com/item/public_token/exchange', {
-  "client_id": process.env.PLAID_CLIENT_ID,
-  "secret": process.env.PLAID_SECRET,
-  "public_token": "public-sandbox-3ef5a951-f351-4fc3-9073-5d3934397a10"
-})
-.then((response) => {
-  console.log(response);
-})
-.catch((err) => console.error(err));
-
+  console.log('inside app get');
+  console.log('req:', req);
+});
 
 // const PORT = 8080;
 
 sequelize.sync().then(() => {
   app.listen(process.env.PORT, () => {
-    console.log(`Your app is manifesting on port ${process.env.PORT}!`)
+    console.log(`Your app is manifesting on port ${process.env.PORT}!`);
   });
 });

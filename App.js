3cbddfa2fe96ffee8859d06578851716aa237b60
Charles Teamer <cmo1462@gmail.com>
2019-08-09 12:42:17 -0500

    lunchtimw

--- App.js
+++ App.js
@@ -28,132 +28,138 @@ import GoalsScreen from './screens/GoalsScreen';
 import { FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET } from './app.config.json';
 // import Geolocation from 'react-native-geolocation-service';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isReady: false,
-      isAuthenticated: false,
-      latitude: null,
-      longitude: null,
-      auth0_id: null,
-      picture: null,
-      name: null,
-    };
-    this.setJwt = this.setJwt.bind(this);
-    // this.DrawerNavigator = DrawerNavigator.bind(this);
-  }
 
-  async componentWillMount() {
-    // GET LOCATION PERMISSIONS:
-    async function getLocationAsync() {
-      // permissions returns only for location permissions on iOS and under certain conditions, see Permissions.LOCATION
-      const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
-      if (status === 'granted') {
-        return navigator.geolocation.watchPosition(
-          position => {
-            console.log(position);
-          },
-          (err) => console.error(err),
-          { timeout: 2000, maximumAge: 2000, enableHighAccuracy: true, distanceFilter: 1 }
-        );
-      } 
-        throw new Error('Location permission not granted');
-      
-    }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isReady: false,
+//       isAuthenticated: false,
+//       latitude: null,
+//       longitude: null,
+//       auth0_id: null,
+//       picture: null,
+//       name: null,
+//     };
+//     this.setJwt = this.setJwt.bind(this);
+//     // this.DrawerNavigator = DrawerNavigator.bind(this);
+//   }
 
-    // setInterval(() => {
-    navigator.geolocation.watchPosition(
-      position => {
-        console.log('position outside of permissions', position);
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-        // fetch(`https://api.foursquare.com/v2/venues/search?client_id=${FOURSQUARE_CLIENT_ID}&client_secret=${FOURSQUARE_CLIENT_SECRET}&ll=${latitude},${longitude}&intent=checkin&radius=60&categoryId=4bf58dd8d48988d1e0931735&v=20190425`)
-        //   .then(result => {
-        //     console.log('get location result from front:', result);
-        //     return result.json();
-        //   })
-        //   .then(response => {
-        //     console.log('response:', response);
-        //   })
-        //   .catch(err => {
-        //     console.log('get location error from front:', err);
-        //   })
-        // PushNotificationIOS.presentLocalNotification({alertBody: 'but does it work?'});
-      },
-      err => console.error(err),
-      { enableHighAccuracy: true, timeout: 2000, maximumAge: 2000, distanceFilter: 0 }
-    );
-    // }, 20000);
+//   async componentDidMount() {
 
-    getLocationAsync();
-    
-    await Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
-    });
-    this.setState({ isReady: true });
-  }
+//     // GET LOCATION PERMISSIONS:
+//     async function getLocationAsync() {
+//       // permissions returns only for location permissions on iOS and under certain conditions, see Permissions.LOCATION
+//       const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
+//       if (status === 'granted') {
+//         return navigator.geolocation.watchPosition(
+//           (position) => {
+//             console.log(position);
+//           },
+//           (err) => console.error(err),
+//           { timeout: 2000, maximumAge: 2000, enableHighAccuracy: true, distanceFilter: 1 }
+//         );
+//       }
+//       throw new Error('Location permission not granted');
+//     }
 
-  setJwt({ name, auth0_id, picture }) {
-    this.setState({ name, auth0_id, picture });
-  }
+//     //setInterval(() => {
+//     navigator.geolocation.watchPosition(
+//       (position) => {
+//         console.log('position outside of permissions', position);
+//         this.setState({
+//           latitude: position.coords.latitude,
+//           longitude: position.coords.longitude,
+//         })
+//         // fetch(`https://api.foursquare.com/v2/venues/search?client_id=${FOURSQUARE_CLIENT_ID}&client_secret=${FOURSQUARE_CLIENT_SECRET}&ll=${latitude},${longitude}&intent=checkin&radius=60&categoryId=4bf58dd8d48988d1e0931735&v=20190425`)
+//             //   .then(result => {
+//             //     console.log('get location result from front:', result);
+//             //     return result.json();
+//             //   })
+//             //   .then(response => {
+//             //     console.log('response:', response);
+//             //   })
+//             //   .catch(err => {
+//             //     console.log('get location error from front:', err);
+//             //   })
+//         // PushNotificationIOS.presentLocalNotification({alertBody: 'but does it work?'});
+//       },
+//       (err) => console.error(err),
+//       { enableHighAccuracy: true, timeout: 2000, maximumAge: 2000, distanceFilter: 0 }
+//     );
+//     //}, 20000);
 
-  render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
-    return (
-      <Container>
-        <DrawerNavigator />
-      </Container>
-    );
-  }
-}
+//     getLocationAsync();
+//     // WATCH CURRENT POSITION:
+//     this.setState({ isReady: true });
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 0,
-    backgroundColor: '#fff',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 50,
-  },
-  message: {
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  basicButton: {
-    backgroundColor: '#49d5b6',
-  },
-  buttonText: {
-    fontWeight: 'bold',
-    color: '#fff',
-  },
-  footerbar: {
-    backgroundColor: '#49d5b6',
-    fontWeight: 'bold',
-    color: '#49d5b6',
-  },
-});
+//   }
 
-const HomeScreenContainer = () => {
-  return HomeScreen;
-};
-const PlaidLinkContainer = () => {
-  return <PlaidScreen />;
-};
+//   setJwt({ name, auth0_id, picture }) {
+//     this.setState({ name, auth0_id, picture });
+//   }
 
-const SignupScreenContainer = () => {
-  // const { name } = this.state;
-  // const LoginScreen = () => <LoginScreen name={this.state.name} setJwt={this.setJwt}/>
-  return SignupScreen;
-};
+//   render() {
+//     if (!this.state.isReady) {
+//       return <AppLoading />;
+//     }
+//     return (
+//       <Container>
+//         <DrawerNavigator />
+//       </Container>
+//     )
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+
+//     justifyContent: "center",
+//     alignItems: 'center',
+//     marginHorizontal: 0,
+//     backgroundColor: '#fff',
+//   },
+//   title: {
+//     fontWeight: 'bold',
+//     fontSize: 50,
+//   },
+//   message: {
+//     fontWeight: 'bold',
+//     fontSize: 20,
+//   },
+//   basicButton: {
+//     backgroundColor: '#34d1af',
+//   },
+//   buttonText: {
+//     fontWeight: 'bold',
+//     color: '#fff',
+//   },
+//   footerbar: {
+//     backgroundColor: '#49d5b6',
+//     fontWeight: 'bold',
+//     color: '#fff',
+//   }
+// });
+
+// const HomeScreenContainer = () => {
+//   return (
+//     HomeScreen
+//   )
+// }
+// const PlaidLinkContainer = () => {
+//     return (
+//       <PlaidScreen />
+//     );
+//   }
+
+// const SignupScreenContainer = () => {
+//   // const { name } = this.state;
+//   // const LoginScreen = () => <LoginScreen name={this.state.name} setJwt={this.setJwt}/>
+//   return (
+//     SignupScreen
+//   )
+// }
+
 
 // class LoginScreen extends React.Component {
 //   render() {
@@ -170,23 +176,22 @@ const SignupScreenContainer = () => {
 //   }
 // }
 
-const DrawerNavigator = createDrawerNavigator(
-  {
-    Home: HomeScreenContainer(),
-    PlaidLink: PlaidScreen,
-    Login: LoginScreen,
-    Signup: SignupScreenContainer(),
-    Goals: GoalsScreen,
-  },
-  {
-    hideStatusBar: true,
-    drawerBackgroundColor: 'rgba(255,255,255,.9)',
-    overlayColor: '#49d5b6',
-    contentOptions: {
-      activeTintColor: '#fff',
-      activeBackgroundColor: '#49d5b6',
-    },
-  }
-);
+// const DrawerNavigator = createDrawerNavigator(
+//   {
+//     Home: HomeScreenContainer(),
+//     PlaidLink: PlaidScreen,
+//     Login: LoginScreen,
+//     Signup: SignupScreenContainer(),
+//   },
+//   {
+//     hideStatusBar: true,
+//     drawerBackgroundColor: 'rgba(255,255,255,.9)',
+//     overlayColor: '#49d5b6',
+//     contentOptions: {
+//       activeTintColor: '#fff',
+//       activeBackgroundColor: '#49d5b6',
+//     },
+//   }
+// );
 
-export default createAppContainer(DrawerNavigator);
+// export default createAppContainer(DrawerNavigator);
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createDrawerNavigator,
+  createAppContainer,
+} from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
+import SignupScreen from '../screens/SignupScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import PlaidScreen from '../screens/PlaidScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+// Implementation of HomeScreen, OtherScreen, SignInScreen, AuthLoadingScreen
+// goes here.
+
+const AppStack = createDrawerNavigator(
+  {
+    Home: HomeScreen,
+    Plaid: PlaidScreen,
+    Settings: SettingsScreen,
+  },
+  {
+    hideStatusBar: true,
+    drawerBackgroundColor: 'rgba(255,255,255,.9)',
+    overlayColor: '#49d5b6',
+    contentOptions: {
+      activeTintColor: '#fff',
+      activeBackgroundColor: '#49d5b6',
+    },
+  }
+);
+const AuthStack = createStackNavigator({ Signup: SignupScreen, login: LoginScreen });
 
 export default createAppContainer(
-  createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
+  createSwitchNavigator(
+    {
+      // AuthLoading: AuthLoadingScreen,
+      App: AppStack,
+      Auth: AuthStack,
+    },
+    {
+      initialRouteName: 'Auth',
+    }
+  )
 );

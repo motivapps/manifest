1b49424c48bb0a471d3cdb4918f95ffa3ef1ff0f
clydebates <a.bates1993@gmail.com>
2019-08-09 18:58:13 -0500

    Beginning of Goals Screen

---
+++ screens/GoalsScreen.js
@@ -0,0 +1,81 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import { StyleSheet, View, Alert, TouchableOpacity } from 'react-native';
+import { Container, Footer, FooterTab, Icon, Content, Button, Text } from 'native-base';
+import Link from './subViews/PlaidLink';
+
+class GoalsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+       <View style={{ flex: 1 }}>
+        <Container style={styles.Container}>
+          <Text style={{ fontWeight: 'bold', marginTop: 20, alignContent: 'center' }}>
+            My Goals
+          </Text>
+        </Container>
+        <Container>
+          <Button style={styles.basicButton} onPress={this.onToggleButton}>
+            <Text style={styles.buttonText}>Current Goals</Text>
+          </Button>
+        </Container>
+        <Content />
+        <Footer style={styles.footerbar}>
+          <FooterTab style={{ backgroundColor: '#49d5b6' }}>
+            <Button vertical>
+              <Icon style={{ fontSize: 30, color: '#fff' }} name="md-stats" />
+              <Text style={styles.buttonText}>Stats</Text>
+            </Button>
+            <Button vertical>
+              <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
+                <Icon style={{ fontSize: 30, color: '#fff' }} name="logo-game-controller-a" />
+                <Text style={styles.buttonText}>Games</Text>
+              </TouchableOpacity>
+            </Button>
+            <Button vertical>
+              <Icon style={{ fontSize: 30, color: '#fff' }} name="md-ribbon" />
+              <Text style={styles.buttonText}>Goals</Text>
+            </Button>
+            <Button vertical>
+              <TouchableOpacity onPress={this.props.navigation.openDrawer}>
+                <Icon style={{ fontSize: 30, color: '#fff' }} name="md-menu" />
+                <Text style={styles.buttonText}>Menu</Text>
+              </TouchableOpacity>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 40,
+  },
+  basicButton: {
+    backgroundColor: '#34d1af',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  footerbar: {
+    backgroundColor: '#49d5b6',
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
+
+export default GoalsScreen;

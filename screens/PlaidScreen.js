52ec79bdd70bb3833cdbcbd3f2ddda9469c90a00
clydebates <a.bates1993@gmail.com>
2019-08-09 18:57:52 -0500

    Fix Android footer color

--- screens/PlaidScreen.js
+++ screens/PlaidScreen.js
@@ -1,71 +1,72 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { StyleSheet, View, Alert, TouchableOpacity } from 'react-native';
 import { Container, Footer, FooterTab, Icon, Content, Button, Text } from 'native-base';
 import Link from './subViews/PlaidLink';
 
 class PlaidScreen extends React.Component {
-    constructor(props) {
-        super(props);
-       
-    }
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Link />
-                <Content />
-                <Footer style={styles.footerbar}>
-                    <FooterTab>
-                        <Button vertical>
-                            <Icon style={{ fontSize: 30, color: '#fff' }} name="md-stats" />
-                            <Text style={styles.buttonText}>Stats</Text>
-                        </Button>
-                        <Button vertical>
-                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
-                                <Icon style={{ fontSize: 30, color: '#fff' }} name="logo-game-controller-a" />
-                                <Text style={styles.buttonText}>Games</Text>
-                            </TouchableOpacity>
-                        </Button>
-                        <Button vertical>
-                            <Icon style={{ fontSize: 30, color: '#fff' }} name="md-ribbon" />
-                            <Text style={styles.buttonText}>Goals</Text>
-                        </Button>
-                        <Button vertical>
-                            <TouchableOpacity onPress={this.props.navigation.openDrawer}>
-                                <Icon style={{ fontSize: 30, color: '#fff' }} name="md-menu" />
-                                <Text style={styles.buttonText}>Menu</Text>
-                            </TouchableOpacity>
-                        </Button>
-                    </FooterTab>
-                </Footer>
-            </View>
-        );
-     }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Link />
+        <Content />
+        <Footer style={styles.footerbar}>
+          <FooterTab style={{backgroundColor: '#49d5b6'}}>
+            <Button vertical>
+              <Icon style={{ fontSize: 30, color: '#fff' }} name="md-stats" />
+              <Text style={styles.buttonText}>Stats</Text>
+            </Button>
+            <Button vertical>
+              <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
+                <Icon style={{ fontSize: 30, color: '#fff' }} name="logo-game-controller-a" />
+                <Text style={styles.buttonText}>Games</Text>
+              </TouchableOpacity>
+            </Button>
+            <Button vertical>
+              <Icon style={{ fontSize: 30, color: '#fff' }} name="md-ribbon" />
+              <Text style={styles.buttonText}>Goals</Text>
+            </Button>
+            <Button vertical>
+              <TouchableOpacity onPress={this.props.navigation.openDrawer}>
+                <Icon style={{ fontSize: 30, color: '#fff' }} name="md-menu" />
+                <Text style={styles.buttonText}>Menu</Text>
+              </TouchableOpacity>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        textAlign: 'center',
-        marginTop: 40,
-    },
-    basicButton: {
-        backgroundColor: '#34d1af',
-    },
-    buttonText: {
-        fontWeight: 'bold',
-        color: '#fff',
-    },
-    footerbar: {
-        backgroundColor: '#49d5b6',
-        fontWeight: 'bold',
-        color: '#fff',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 40,
+  },
+  basicButton: {
+    backgroundColor: '#34d1af',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  footerbar: {
+    backgroundColor: '#49d5b6',
+    fontWeight: 'bold',
+    color: '#fff',
+  },
 });
 
 export default PlaidScreen;

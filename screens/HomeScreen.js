52865f757780ad7bea00780f51f5f0abaa62b95a
Rachel Brandsness <brandsnessr@gmail.com>
2019-08-08 19:02:54 -0500

    Refactor client to separate components from App

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,253 +1,104 @@
-import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { AppLoading } from 'expo';
+import { Container, Text, Button, Footer, FooterTab, Icon, Content } from 'native-base';
+import { Platform, StatusBar, StyleSheet, View, TouchableOpacity, PushNotificationIOS } from 'react-native';
+
 
 import { MonoText } from '../components/StyledText';
 
-export default function HomeScreen() {
-  return (
-    <button id="link-button">Link Account</button>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/
-  jquery/2.2.3/jquery.min.js"></script>
-<script src="https://cdn.plaid.com/link/v2/
-  stable/link-initialize.js">
-  </script>
-<script type="text/javascript">
-(function($) {
-  var handler = Plaid.create({
-    clientName: 'Plaid Quickstart',
-    env: '<%= PLAID_ENV %>',
-    key: '<%= PLAID_PUBLIC_KEY %>',
-    product: ['transactions'],
-    // Optional – use webhooks to get transaction and error updates
-    webhook: 'https://requestb.in',
-    onLoad: function() {
-      // Optional, called when Link loads
-    },
-    onSuccess: function(public_token, metadata) {
-      // Send the public_token to your app server.
-      // The metadata object contains info about the institution the
-      // user selected and the account ID or IDs, if the
-      // Select Account view is enabled.
-      $.post('/get_access_token', {
-        public_token: public_token,
-      });
-    },
-    onExit: function(err, metadata) {
-      // The user exited the Link flow.
-      if (err != null) {
-        // The user encountered a Plaid API error prior to exiting.
-      }
-      // metadata contains information about the institution
-      // that the user selected and the most recent API request IDs.
-      // Storing this information can be helpful for support.
-    },
-    onEvent: function(eventName, metadata) {
-      // Optionally capture Link flow events, streamed through
-      // this callback as your users connect an Item to Plaid.
-      // For example:
-      // eventName = "TRANSITION_VIEW"
-      // metadata  = {
-      //   link_session_id: "123-abc",
-      //   mfa_type:        "questions",
-      //   timestamp:       "2017-09-14T14:42:19.350Z",
-      //   view_name:       "MFA",
-      // }
-    }
-  });
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+      buttonToggle: false,
+      isAuthenticated: false,
+      
+    };
+    this.onToggleButton = this.onToggleButton.bind(this);
+    this.setState = this.setState.bind(this);
+  }
 
-  $('#link-button').on('click', function(e) {
-    handler.open();
-  });
-})(jQuery);
-</script>
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+ 
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
+  onToggleButton() {
+    this.setState({
+      buttonToggle: !this.state.buttonToggle,
+    })
+  }
 
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            But does it work????
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
+  render() {
 
     return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
+      <Container style={styles.container}>
+
+        <Text style={{ fontWeight: 'bold', marginTop: 20 }}>Home</Text>
+        <Text style={styles.title}>Manifest</Text>
+        <Text>It's alive!</Text>
+        <Text>My Latitude: {this.state.latitude}</Text>
+        <Text>My Longitude: {this.state.longitude}</Text>
+        <Button style={styles.basicButton} onPress={this.onToggleButton}>
+          <Text style={styles.buttonText}>Do Not Click Me!</Text>
+        </Button>
+        {this.state.buttonToggle ? <Text style={styles.message}>I said don't click me!</Text> : null}
+        <Content />
+        <Footer style={styles.footerbar}>
+          <FooterTab>
+            <Button vertical>
+              <Icon style={{ fontSize: 30, color: '#fff' }} name="md-stats" />
+              <Text style={styles.buttonText}>Stats</Text>
+            </Button>
+            <Button vertical>
+              <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
+                <Icon style={{ fontSize: 30, color: '#fff' }} name="logo-game-controller-a" />
+                <Text style={styles.buttonText}>Games</Text>
+              </TouchableOpacity>
+            </Button>
+            <Button vertical>
+              <Icon style={{ fontSize: 30, color: '#fff' }} name="md-ribbon" />
+              <Text style={styles.buttonText}>Goals</Text>
+            </Button>
+            <Button vertical>
+              <TouchableOpacity onPress={this.props.navigation.openDrawer}>
+                <Icon style={{ fontSize: 30, color: '#fff' }} name="md-menu" />
+                <Text style={styles.buttonText}>Menu</Text>
+              </TouchableOpacity>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+
+    justifyContent: "center",
+    alignItems: 'center',
+    marginHorizontal: 0,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  title: {
+    fontWeight: 'bold',
+    fontSize: 50,
   },
-  contentContainer: {
-    paddingTop: 30,
+  message: {
+    fontWeight: 'bold',
+    fontSize: 20,
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+  basicButton: {
+    backgroundColor: '#34d1af',
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#fff',
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  footerbar: {
+    backgroundColor: '#49d5b6',
+    fontWeight: 'bold',
+    color: '#fff',
+  }
 });
+
+export default HomeScreen;

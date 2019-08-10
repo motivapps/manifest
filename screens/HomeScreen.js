885ef96ee54bf44e329054459d05035323ec642a
Rachel Brandsness <brandsnessr@gmail.com>
2019-08-10 15:12:43 -0500

    Create UI's for transactions and start to create HomeScreen UI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -165,22 +165,22 @@ class HomeScreen extends React.Component {
     }
     return (
       <Container style={styles.container}>
-        <Text style={{ fontWeight: 'bold', marginTop: 20 }}>Home</Text>
+      <View style={styles.viewport}>
         <Text style={styles.title}>Manifest</Text>
-        <Text>It's alive!</Text>
-        <Text>My Latitude: {this.state.latitude}</Text>
-        <Text>My Longitude: {this.state.longitude}</Text>
-        <Button style={styles.basicButton} onPress={this.onToggleButton}>
-          <Text style={styles.buttonText}>Do Not Click Me!</Text>
-        </Button>
-        {this.state.buttonToggle ? (
-          <Text style={styles.message}>I said don't click me!</Text>
-        ) : null}
+        <Text style={styles.heading}>Goal: Fancy Pants Kayak</Text>
+
         <Image
-          style={{ width: 50, height: 50 }}
-          source={kayak}
+          style={styles.mainImage}
+          source={require('../assets/images/kayak.jpg')}
         />
-        <Content />
+        <Text style={styles.smallText}>Projected Completion Date: 9/14/19</Text>
+        <Text style={styles.largeText}>Money Saved: $248</Text>
+        <Text style={styles.largeText}>Current Streak: 79 days</Text>
+        <Text style={styles.smallTextLeft}>Relapses: 3</Text>
+        <Text style={styles.smallTextLeft}>Money Lost: $22.35</Text>
+        <Text style={styles.smallTextLeft}>Setback: 6 days</Text>
+        </View>
+        
         <Footer style={styles.footerbar}>
           <FooterTab style={{ backgroundColor: '#49d5b6' }}>
             <Button vertical>
@@ -219,16 +219,50 @@ const styles = StyleSheet.create({
     marginHorizontal: 0,
     backgroundColor: '#fff',
   },
+  viewport: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
   title: {
     fontWeight: 'bold',
-    fontSize: 50,
+    fontSize: 30,
+    color: '#49d5b6',
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    color: '#49d5b6',
+    marginBottom: 20,
+  },
+  largeText: {
+    fontWeight: 'bold',
+    fontSize: 22,
+    color: '#49d5b6',
+    alignSelf: 'flex-start',
+    marginLeft: 0,
+  },
+  smallText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#4c4c4c',
+  },
+  smallTextLeft: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#4c4c4c',
+    alignSelf: 'flex-start',
+    marginLeft: 0,
   },
   message: {
     fontWeight: 'bold',
     fontSize: 20,
   },
   basicButton: {
-    backgroundColor: '#34d1af',
+    backgroundColor: '#49d5b6',
   },
   buttonText: {
     fontWeight: 'bold',
@@ -238,6 +272,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#49d5b6',
     fontWeight: 'bold',
     color: '#fff',
+  },
+  mainImage: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#49d5b6',
+    margin: 10,
+    borderRadius: '100%',
+    borderWidth: 4,
+    borderColor: '#49d5b6',
   },
 });
 
--- screens/TransactionsScreen.js
+++ screens/TransactionsScreen.js
@@ -1,14 +1,75 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View, TouchableOpacity } from 'react-native';
-import { Container, Text, Button, Footer, FooterTab, Icon, Content } from 'native-base';
+import { Platform, StatusBar, StyleSheet, View, TouchableOpacity, ScrollView } from 'react-native';
+import {
+  Container,
+  Text,
+  Button,
+  Footer,
+  FooterTab,
+  Icon,
+  Content,
+  Grid,
+  Col,
+  Row,
+} from 'native-base';
+import TransactionItem from './subViews/TransactionItem';
 
 export default class Transactions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: [
+        {
+          id: 1,
+          id_user: 4,
+          status: 'pending',
+          name: 'Starbucks',
+          day: '2019-07-17T11:00:00.000Z',
+          amount: '4.33',
+          transaction_id: 'P3voBrVXlBFqMB7ya1bdFRko8nQo1pt7lBK8r',
+          createdAt: '2019-08-10T17:22:55.904Z',
+          updatedAt: '2019-08-10T17:22:55.904Z',
+        },
+        {
+          id: 1,
+          id_user: 4,
+          status: 'pending',
+          name: "CC's",
+          day: '2019-07-18T02:37:00.000Z',
+          amount: '3.99',
+          transaction_id: 'P3voBrVXlBFqMB7ya1bdFRko8nQo1pt7lBK8r',
+          createdAt: '2019-08-10T17:22:55.904Z',
+          updatedAt: '2019-08-10T17:22:55.904Z',
+        },
+        {
+          id: 1,
+          id_user: 4,
+          status: 'pending',
+          name: "Starbucks",
+          day: '2019-07-18T08:12:00.000Z',
+          amount: '4.99',
+          transaction_id: 'P3voBrVXlBFqMB7ya1bdFRko8nQo1pt7lBK8r',
+          createdAt: '2019-08-10T17:22:55.904Z',
+          updatedAt: '2019-08-10T17:22:55.904Z',
+        },
+      ],
+    };
+  }
+
   render() {
+    const { transactions } = this.state;
+
     return (
-      <Container style={this.styles.container}>
-        <View style={this.styles.container}>
-          <Text style={{ fontWeight: 'bold', marginTop: 20 }}>Transactions</Text>
+      <Container style={styles.container}>
+        <View style={styles.viewport}>
+          <Text style={styles.heading}>Transactions</Text>
+          <Text style={styles.smallTextGreen}>
+            These transactions look a little suspicious... Still sticking to your goals?
+          </Text>
+            {transactions.map(transaction => (
+              <TransactionItem transaction={transaction} />
+            ))}
         </View>
         <Footer style={styles.footerbar}>
           <FooterTab>
@@ -44,9 +105,76 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  viewport: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+  },
   title: {
-    fontSize: 20,
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#49d5b6',
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    color: '#49d5b6',
+    marginBottom: 10,
+    marginTop: 12,
+  },
+  largeText: {
+    fontWeight: 'bold',
+    fontSize: 22,
+    color: '#49d5b6',
+    alignSelf: 'flex-start',
+    marginLeft: 0,
+  },
+  smallText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#4c4c4c',
+  },
+  smallTextLeft: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#4c4c4c',
+    alignSelf: 'flex-start',
+  },
+  smallTextGreen: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#49d5b6',
     textAlign: 'center',
-    marginTop: 40,
+  },
+  smallTextLeft: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#4c4c4c',
+    alignSelf: 'flex-start',
+    marginLeft: 0,
+  },
+  transactionButton: {
+    backgroundColor: '#49d5b6',
+    height: 60,
+    alignSelf: 'flex-start',
+    maxWidth: '96%',
+    width: '96%',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    width: '100%',
+  },
+  transactionColumns: {
+    backgroundColor: '#fff',
+  },
+  footerbar: {
+    backgroundColor: '#49d5b6',
+    fontWeight: 'bold',
+    color: '#fff',
   },
 });

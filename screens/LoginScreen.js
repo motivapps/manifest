5e72ee6489ff52d22bb86558a6bb1c33a3c11f9d
Charles Teamer <cmo1462@gmail.com>
2019-08-15 16:58:41 -0500

    Merged signup and login into signup

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -15,73 +15,49 @@ function toQueryString(params) {
 }
 
 class LoginScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: null,
-    };
-    this.storeData = this.storeData.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     name: null,
+  //   };
+  //   this.storeData = this.storeData.bind(this);
+  // }
 
-  componentDidUpdate() {
-    // const { auth0_id } = this.state;
+  // componentDidUpdate() {
+  //   // const { auth0_id } = this.state;
 
-    if (this.state.name) {
-      this.storeData(this.state.auth0_id);
-      // this.props.navigation.dispatch(SwitchActions.jumpTo({ routeName: 'App' }));
-      this.props.navigate('App');
-    }
-  }
+  //   if (this.state.name) {
+  //     this.storeData(this.state.auth0_id);
+  //     // this.props.navigation.dispatch(SwitchActions.jumpTo({ routeName: 'App' }));
+  //     this.props.navigate('App');
+  //   }
+  // }
 
-  storeData = async token => {
-    try {
-      await AsyncStorage.setItem('userToken', token);
-    } catch (error) {
-      navigation.navigate('Signup');
-    }
-  };
+  // storeData = async token => {
+  //   try {
+  //     await AsyncStorage.setItem('userToken', token);
+  //   } catch (error) {
+  //     navigation.navigate('Signup');
+  //   }
+  // };
 
-  login = async () => {
-    // Retrieve the redirect URL, add this to the callback URL list
-    // of your Auth0 application.
-    const redirectUrl = AuthSession.getRedirectUrl();
-    console.log(`Redirect URL: ${redirectUrl}`);
+  
 
-    // Structure the auth parameters and URL
-    const queryParams = toQueryString({
-      client_id: AUTHO_CLIENT_ID,
-      redirect_uri: redirectUrl,
-      response_type: 'id_token', // id_token will return a JWT token
-      scope: 'openid profile', // retrieve the user's profile
-      nonce: 'nonce', // ideally, this will be a random value
-    });
-    const authUrl = `${AUTHO_DOMAIN}/authorize${queryParams}`;
-    console.log('authURL', authUrl);
+  // handleResponse = response => {
+    // if (response.error) {
+    //   Alert('Authentication error', response.error_description || 'something went wrong');
+    //   return;
+    // }
 
-    // Perform the authentication
-    const response = await AuthSession.startAsync({ authUrl });
-    console.log('Authentication response', response);
+    // // Retrieve the JWT token and decode it
+    // const jwtToken = response.id_token;
+    // const { name, sub } = jwtDecode(jwtToken);
+    // this.setState({ name, auth0_id: sub });
 
-    if (response.type === 'success') {
-      this.handleResponse(response.params);
-    }
-  };
+    // console.log('JWTotken data', jwtDecode(jwtToken));
 
-  handleResponse = response => {
-    if (response.error) {
-      Alert('Authentication error', response.error_description || 'something went wrong');
-      return;
-    }
-
-    // Retrieve the JWT token and decode it
-    const jwtToken = response.id_token;
-    const { name, sub } = jwtDecode(jwtToken);
-    this.setState({ name, auth0_id: sub });
-
-    console.log('JWTotken data', jwtDecode(jwtToken));
-
-    axios.post(`${NGROK}/login`, { auth0_id: sub });
-  };
+    // axios.post(`${NGROK}/login`, { auth0_id: sub });
+  // };
 
   render() {
     const { name } = this.state;

544d9395a242c4c14d45bbdac2f9fe72f43a76fd
Rachel Brandsness <brandsnessr@gmail.com>
2019-08-17 18:28:45 -0500

    Start to make shell of game frame and controls component

---
+++ screens/games/GameTwo/Controls.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Accelerometer, GLView } from 'expo';
+import PropTypes from 'prop-types';
+import { MultiTouchView } from 'expo-multi-touch';
--- screens/games/GameTwo/GameTwo.js
+++ screens/games/GameTwo/GameTwo.js
@@ -1,24 +1,92 @@
 import React from 'react';
 import {
- StyleSheet, Text, TouchableOpacity, View 
+  StyleSheet, Text, TouchableOpacity, View,
 } from 'react-native';
 import { AppLoading, Constants, ScreenOrientation } from 'expo';
+import Controls from './Controls';
 
-ScreenOrientation.allowAsync(ScreenOrientation.Orientation.PORTRAIT);
+ScreenOrientation.lockAsync(ScreenOrientation.Orientation.PORTRAIT);
 
 export default class GameTwo extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      gamePause: false,
+      pause: false,
       isLoading: true,
       score: 0,
+      lives: 3,
     };
+    this.onPauseGame = this.onPauseGame.bind(this);
   }
+
+  onPauseGame() {
+    this.setState(prevState => ({
+      pause: !prevState.pause,
+    }));
+  }
+
   render() {
+    const { pause, isLoading, score, lives } = this.state;
+
+    // if (isLoading) {
+    //   return (
+    //     <AppLoading
+    //       onFinish={() => this.setState({ isLoading: false })}
+    //       // startAsync={this.preloadAssetsAsync}
+    //     />
+    //   );
+    // }
+
     return (
-      <Text>GameTwo</Text>
+      <React.Fragment>
+      <View style={styles.container}>
+        <Text style={styles.text}>{`Score: ${score}`}</Text>
+          <Text style={styles.text}>{`Lives Remaining: ${lives}`}</Text>
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={this.onPauseGame}
+          style={styles.footerButton}
+        >
+          <Text style={styles.footerText}>
+            {pause ? 'Play' : 'Pause'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+      </React.Fragment>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    left: 16,
+    position: 'absolute',
+    marginTop: 50,
+    width: '100%',
+  },
+  text: {
+    color: '#000',
+  },
+  footer: {
+    alignItems: 'center',
+    bottom: 32,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    position: 'absolute',
+    width: '100%',
+  },
+  footerButton: {
+    backgroundColor: '#4c4c4c',
+    borderRadius: 8,
+    marginHorizontal: 16,
+    padding: 16,
+    width: 76,
+  },
+  footerText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+});

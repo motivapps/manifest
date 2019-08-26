902f9797ccf2a653b0e38dff289d502f007b6259
Charles Teamer <cmo1462@gmail.com>
2019-08-26 11:44:58 -0500

    New file for goalstack

---
+++ navigation/subStacks/GoalStack.js
@@ -0,0 +1,35 @@
+import { createStackNavigator } from 'react-navigation';
+import defaultHeader from './DefaultHeader';
+import GoalsScreen from '../../screens/GoalsScreen';
+import GoalsSummaryScreen from '../../screens/GoalsSummaryScreen';
+
+const _ = require('lodash');
+
+const GoalsStack = createStackNavigator(
+  {
+    Goals: {
+      screen: GoalsScreen,
+    },
+    GoalSummary: {
+      screen: GoalsSummaryScreen,
+    },
+  },
+  _.merge({
+    initialRouteName: 'Goals',
+    initialRouteParams: {
+      auth: false,
+    },
+  }, defaultHeader),
+);
+
+GoalsStack.navigationOptions = {
+  tabBarLabel: 'Goals',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
+  ),
+};
+
+GoalsStack.path = 'app/goals';
+
+
+export default GoalsStack;

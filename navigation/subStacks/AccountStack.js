c8c82e18763e33045de4343fd863bf574b8cc904
Charles Teamer <cmo1462@gmail.com>
2019-08-26 11:57:26 -0500

    Added screen for account stack

---
+++ navigation/subStacks/AccountStack.js
@@ -0,0 +1,40 @@
+import { createStackNavigator } from 'react-native';
+import TabBarIcon from '../../components/TabBarIcon';
+import PlaidScreen from '../../screens/PlaidScreen';
+import MyAccountScreen from '../../screens/MyAccountScreen';
+import AccountAssignScreen from '../../screens/AccountAssignScreen';
+import defaultHeader from './DefaultHeader';
+
+const _ = require('lodash');
+
+
+const AccountStack = createStackNavigator(
+  {
+    MyAccount: {
+      screen: MyAccountScreen,
+    },
+    Plaid: {
+      screen: PlaidScreen,
+    },
+    AccountAssign: {
+      screen: AccountAssignScreen,
+    },
+  },
+  _.merge({
+    initialRouteName: 'MyAccount',
+    initialRouteParams: {
+      auth: false,
+    },
+  }, defaultHeader),
+);
+
+AccountStack.navigationOptions = {
+  tabBarLabel: 'Account',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+  ),
+};
+
+AccountStack.path = 'app/my-account';
+
+export default AccountStack;

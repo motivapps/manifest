3620ba9002e8fa14a92164ffe542c246e46377c0
Charles Teamer <cmo1462@gmail.com>
2019-08-26 12:15:32 -0500

    Added GamesStack

---
+++ navigation/subStacks/GamesStack.js
@@ -0,0 +1,43 @@
+import { createStackNavigator } from 'react-native';
+import TabBarIcon from '../../components/TabBarIcon';
+import defaultHeader from './DefaultHeader';
+import GamesScreen from '../../screens/GamesScreen';
+import DKScreen from '../../screens/games/DK';
+import GameTwoScreen from '../../screens/games/GameTwo';
+import GameThreeScreen from '../../screens/games/GameThree';
+import GameFourScreen from '../../screens/games/GameFour';
+
+const GamesStack = createStackNavigator(
+  {
+    Goals: {
+      screen: GamesScreen,
+    },
+    DK: {
+      screen: DKScreen,
+    },
+    GameTwo: {
+      screen: GameTwoScreen,
+    },
+    GameThree: {
+      screen: GameThreeScreen,
+    },
+    GameFour: {
+      screen: GameFourScreen,
+    },
+  },
+  defaultHeader,
+);
+
+GamesStack.navigationOptions = {
+  tabBarLabel: 'Games',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon 
+      focused={focused} 
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} 
+      />
+  ),
+};
+
+GamesStack.path = 'app/games';
+
+export default GamesStack;

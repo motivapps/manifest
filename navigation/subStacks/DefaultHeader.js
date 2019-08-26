2219bbe2b863c8205e7ccbefe0eb1628677b4fc0
Charles Teamer <cmo1462@gmail.com>
2019-08-26 11:44:18 -0500

    Made a seperate file for the app header

---
+++ navigation/subStacks/DefaultHeader.js
@@ -0,0 +1,22 @@
+import { Platform } from 'react-native';
+import ManifestTitle from '../../screens/Header';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const defaultHeader = {
+  defaultNavigationOptions: {
+    headerTitle: <ManifestTitle />,
+    headerTintColor: '#fff',
+    headerStyle: {
+      backgroundColor: '#4c4c4c',
+    },
+  },
+  headerLayoutPreset: 'center',
+  config,
+};
+
+
+export default defaultHeader;

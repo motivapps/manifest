79e5d5dc350e29f98199aad3c3ef85c2a5ad27ec
Charles Teamer <cmo1462@gmail.com>
2019-08-26 12:02:04 -0500

    Created Transaction stack

---
+++ navigation/subStacks/TransactionStack.js
@@ -0,0 +1,25 @@
+import { createStackNavigator } from 'react-native';
+import TabBarIcon from '../../components/TabBarIcon';
+import TransactionsScreen from '../../screens/TransactionsScreen';
+import defaultHeader from './DefaultHeader';
+
+const TransactionsStack = createStackNavigator(
+  {
+    Transactions: {
+      screen: TransactionsScreen,
+    },
+  },
+  defaultHeader,
+);
+
+TransactionsStack.navigationOptions = {
+  tabBarLabel: 'Transactions',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon 
+      focused={focused} 
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} 
+    />
+  ),
+};
+
+TransactionsStack.path = 'app/transactions';

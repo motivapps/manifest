54e9b89cd429edfe3c43341febb1e82e47cdadd3
Charles Teamer <cmo1462@gmail.com>
2019-08-27 12:15:54 -0500

    merged the platform select thing so it would be read by default

--- navigation/subStacks/DefaultHeader.js
+++ navigation/subStacks/DefaultHeader.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { Platform } from 'react-native';
 import ManifestTitle from '../../screens/Header';
 
+const _ = require('lodash');
+
 const config = Platform.select({
   web: { headerMode: 'screen' },
   default: {},
 });
 
 const defaultHeader = {
-  defaultNavigationOptions: {
-    headerTitle: <ManifestTitle />,
-    headerTintColor: '#fff',
-    headerStyle: {
-      backgroundColor: '#4c4c4c',
+  defaultNavigationOptions: _.merge(
+    {
+      headerTitle: <ManifestTitle />,
+      headerTintColor: '#fff',
+      headerStyle: {
+        backgroundColor: '#4c4c4c',
+      },
+      headerLayoutPreset: 'center',
     },
-  },
-  headerLayoutPreset: 'center',
-  config,
+    config,
+  ),
 };
 
 

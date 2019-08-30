de8d0ea0a53254a7bf7bc76efe30402493e39cdf
Charles Teamer <cmo1462@gmail.com>
2019-08-30 10:58:41 -0500

    lint

--- screens/Header.js
+++ screens/Header.js
@@ -3,9 +3,8 @@ import { Text } from 'react-native';
 
 export default ManifestTitle = () => (
   <Text
-    style={{ color: '#fff', fontWeight: 'bold', fontSize: 25 }}
+    style={{
+      color: '#fff', fontWeight: 'bold', fontSize: 25 }}
     onPress={() => console.log('Would be nice if this went back to home screen')}
-  >
-    Manifest
-  </Text>
+  >Manifest</Text>
 );
